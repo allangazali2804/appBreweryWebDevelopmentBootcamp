7b6a2e304de79cf145d7ed0933e07c5020bdde53
Allan Gazali <128587014+allangazali2804@users.noreply.github.com>
2023-08-21 19:14:19 +0700

    Add files via upload

---
+++ stringLength/index.js
@@ -0,0 +1,7 @@
+// please run this code in browser javascript, you know how to do it.
+
+var message = "You have written ";
+var name = prompt("Type somehing here!");
+var length = 180 - name.length;
+
+alert(message + name.length + ", you have " + length + " characters left"); 
