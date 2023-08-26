3195c304b9d9dd01843cb73d3f77c6b54c245a7f
Allan Gazali <128587014+allangazali2804@users.noreply.github.com>
2023-08-26 15:15:46 +0700

    Update index.js

--- JavaScript Function karelRobot/index.js
+++ JavaScript Function karelRobot/index.js
@@ -1,3 +1,24 @@
+function getMilk() {   
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHouse");
+}
+
 /**
  * Welcome to the Stanford Karel IDE.
  * This is a free space for you to 
