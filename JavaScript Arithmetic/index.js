35f7cabd55683031382f8491ab9b86d921f756d2
Allan Gazali <128587014+allangazali2804@users.noreply.github.com>
2023-08-29 12:52:48 +0700

    Delete JavaScript Arithmetic directory

--- JavaScript Arithmetic/index.js
+++
@@ -1,9 +0,0 @@
-// Create the variables to store the value
-var dogAge = prompt("How old is your dog?");
-
-/* Add arithmetic operations to get things done and don't forget to add parentheses to all operations that need to be
-executed first */
-var humanAge = ((dogAge-2)*4) +21;
-
-// And this should be the result, run this code in the browser
-alert("Equivalent human age is " + humanAge);
