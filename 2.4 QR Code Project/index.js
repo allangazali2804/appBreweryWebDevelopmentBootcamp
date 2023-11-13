86d4ce5bb902762710efa5cba447b847563b611f
Allan Gazali <128587014+allangazali2804@users.noreply.github.com>
2023-11-13 19:52:46 +0700

    Update index.js

--- 2.4 QR Code Project/index.js
+++ 2.4 QR Code Project/index.js
@@ -19,6 +19,7 @@ inquirer
     var qr_svg = qr.image(url);
     qr_svg.pipe(fs.createWriteStream("qr_img.png"));
 
+     //Creating file txt with fs
     fs.writeFile("URL.txt", url, (err) => {
       if (err) throw err;
       console.log("The file has been saved!");
